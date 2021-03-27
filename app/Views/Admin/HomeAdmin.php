10b9582f359be5d1e6dc2e1f1634c38f91cb8916
I Gede Bagus Wirawan <gusdechang24@gmail.com>
2021-03-27 22:29:21 +0800

    HomeAdmin Page

---
+++ app/Views/Admin/HomeAdmin.php
@@ -0,0 +1,54 @@
+<?php $this->extend('layout/AdminLayout'); ?>
+
+<?php $this->section('content'); ?>
+<main id="main">
+
+    <!-- ======= Breadcrumbs Section ======= -->
+    <section class="breadcrumbs">
+        <div class="container">
+
+            <div class="d-flex justify-content-between align-items-center">
+                <h2>Home Page</h2>
+            </div>
+
+        </div>
+    </section><!-- End Breadcrumbs Section -->
+
+    <!-- ======= Dashboard Section ======= -->
+    <section class="inner-page">
+        <div class="container">
+
+            <div class="section-title">
+                <h2>Home Page</h2>
+            </div>
+
+            <!-- Konten -->
+
+            <!-- alert -->
+            <?php if (session()->getFlashdata('pesan')) : ?>
+            <div class="alert alert-primary alert-dismissible fade show" style="font-size: 14px;" role="alert">
+                <?= session()->getFlashdata('pesan'); ?>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <?php endif; ?>
+            <!-- end alert -->
+
+            <div class="card">
+                <div class="card-body">
+
+                    <!-- Text Area -->
+
+                    <!-- End Text Area -->
+
+                </div>
+            </div>
+            <!-- End Konten -->
+
+        </div>
+    </section>
+    <!-- ======= End Dashboard Section ======= -->
+
+</main><!-- End #main -->
+<?php $this->endSection(); ?>
