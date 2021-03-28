dc37cb386ba7a3f17653ddba47d68306c03d652b
I Gede Bagus Wirawan <gusdechang24@gmail.com>
2021-03-28 22:40:29 +0800

    Misi Table

--- app/Views/Admin/HomeAdmin.php
+++ app/Views/Admin/HomeAdmin.php
@@ -107,9 +107,66 @@
             <div class="card mb-3">
                 <div class="card-body">
 
-                    <!-- Text Area -->
+                    <button style="font-size: 12px;" type="button" class="btn p-1 mb-4 btn-sm btn-primary"
+                        data-toggle="modal" data-target="#MisiModal_C">
+                        Tambah Misi
+                    </button>
 
-                    <!-- End Text Area -->
+                    <!-- modal tambah operator -->
+                    <?php include('Modal/Misi/MisiModal_C.php'); ?>
+                    <!-- end modal tambah operator -->
+
+                    <!-- Misi Table -->
+                    <div class="table-responsive" style="font-size: 14px;">
+                        <table id="datalol" class="table table-striped table-bordered mt-3" style="color:black;">
+                            <thead>
+                                <tr>
+                                    <th scope="col" style="text-align:center;">No</th>
+                                    <th scope="col">Misi</th>
+                                    <th scope="col">Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <!-- looping -->
+                                <?php
+                                    $no=1; 
+                                    foreach($misi as $m) :
+                                ?>
+                                <tr>
+                                    <th scope="row" style="text-align: center;">
+                                        <?= $no++; ?>
+                                    </th>
+                                    <td>
+                                        <?= $m['misi']; ?>
+                                    </td>
+                                    <td>
+                                        <button style="font-size: 12px;" type="button"
+                                            class="btn p-1 btn-sm btn-primary" data-toggle="modal"
+                                            data-target="#MisiModal_U<?= $no ?>">
+                                            Update
+                                        </button>
+
+                                        <!-- modal tambah operator -->
+                                        <?php include('Modal/Misi/MisiModal_U.php'); ?>
+                                        <!-- end modal tambah operator -->
+
+                                        <button style="font-size: 12px;" type="button" class="btn p-1 btn-sm btn-danger"
+                                            data-toggle="modal" data-target="#MisiModal_D<?= $no ?>">
+                                            Delete
+                                        </button>
+
+                                        <!-- modal tambah operator -->
+                                        <?php include('Modal/Misi/MisiModal_D.php'); ?>
+                                        <!-- end modal tambah operator -->
+
+                                    </td>
+                                </tr>
+                                <?php endforeach; ?>
+                                <!-- End looping -->
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- End Misi Table -->
 
                 </div>
             </div>
